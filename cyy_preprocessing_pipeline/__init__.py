f7f257d8f2064b9cad9f450898456b9d4561c4fd
cyy <cyyever@outlook.com>
2025-08-16 18:56:13 +0800

    Fix code

--- cyy_preprocessing_pipeline/__init__.py
+++ cyy_preprocessing_pipeline/__init__.py
@@ -1,10 +1,11 @@
 from .dataset import DatasetWithIndex, get_dataset_size, select_item, subset_dp
 from .pipeline import DataPipeline
-from .transform import BatchTransform, DatasetTransform, Transform
+from .transform import BatchTransform, DatasetTransform, Transform, SampleTransform
 
 __all__ = [
     "Transform",
     "BatchTransform",
+    "SampleTransform",
     "DatasetTransform",
     "DataPipeline",
     "get_dataset_size",
