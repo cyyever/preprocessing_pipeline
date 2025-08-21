5fb5d4de766f8cab4a8f23a6287753e419f45ecf
cyy <cyyever@outlook.com>
2025-08-21 18:05:09 +0800

    Fix code

--- cyy_preprocessing_pipeline/__init__.py
+++ cyy_preprocessing_pipeline/__init__.py
@@ -1,6 +1,6 @@
 from .common import strip_lines
 from .dataset import DatasetWithIndex, get_dataset_size, select_item, subset_dp
-from .json_dataset import incremental_computing, load_json, incremental_reduce
+from .json_dataset import incremental_computing, incremental_reduce, load_json
 from .pipeline import DataPipeline
 from .transform import BatchTransform, DatasetTransform, SampleTransform, Transform
 
--- cyy_preprocessing_pipeline/parsing.py
+++ cyy_preprocessing_pipeline/parsing.py
@@ -1,4 +1,5 @@
 from collections.abc import Sequence
+
 import bs4
 
 
