baef63f4b68f1a484f88b02e44e5346717b8d1bf
cyy <cyyever@outlook.com>
2025-08-24 20:15:57 +0800

    Fix code
    Signed-off-by: cyy <cyyever@outlook.com>

--- cyy_preprocessing_pipeline/incremental_computing.py
+++ cyy_preprocessing_pipeline/incremental_computing.py
@@ -1,31 +1,29 @@
-from collections.abc import Callable
+from collections.abc import Callable, Generator
 from typing import Any
 
+from cyy_naive_lib.concurrency import ProcessPool
 from cyy_naive_lib.storage import load_json, save_json
 from cyy_naive_lib.time_counter import TimeCounter
 
 
 def incremental_save(
     output_json: str,
-    data_fun: Callable[[dict], tuple[Any, Any] | None],
+    data_fun: Callable[[Any], Generator[tuple[str, Any], Any, None]],
     save_second_interval: int = 10 * 60,
 ) -> None:
-    new_value: bool = False
     res = load_json(output_json)
     time_counter = TimeCounter()
-    while True:
-        result_pair = data_fun(res)
-        if result_pair is None:
-            break
+    for result_pair in data_fun(res):
         key, v = result_pair
         assert key not in res
         res[key] = v
-        new_value = True
         if time_counter.elapsed_seconds() >= save_second_interval:
             save_json(res, output_json)
             time_counter.reset_start_time()
-    if new_value:
-        save_json(res, output_json)
+    save_json(res, output_json)
+
+
+executor_pool: ProcessPool | None = None
 
 
 def incremental_computing(
@@ -33,17 +31,38 @@ def incremental_computing(
     output_json: str,
     fun: Callable[[str, Any], tuple[Any, bool]],
     save_second_interval: int = 10 * 60,
+    multiprocessing: bool = False,
 ) -> None:
+    global executor_pool
     data = load_json(input_json)
 
-    def data_fun(res):
-        for sample_id, value in data.items():
-            if str(sample_id) in res:
-                continue
-            result, done = fun(sample_id, value)
-            if done:
-                return str(sample_id), result
+    if multiprocessing and executor_pool is None:
+        executor_pool = ProcessPool()
+
+    def wrapped_fun(sample_id, value):
+        result, done = fun(sample_id, value)
+        if done:
+            return str(sample_id), result
         return None
+
+    def data_fun(previous_results) -> Generator[tuple[str, Any], Any, None]:
+        assert isinstance(previous_results, dict)
+        for sample_id, value in data.items():
+            if str(sample_id) in previous_results:
+                continue
+            if multiprocessing:
+                assert executor_pool is not None
+                executor_pool.submit(wrapped_fun, sample_id, value)
+                futures, _ = executor_pool.wait_results(timeout=0)
+                for tuple_result in futures.values():
+                    if tuple_result is not None:
+                        sample_id, result = tuple_result
+                        assert isinstance(sample_id, str)
+                        yield str(sample_id), result
+            else:
+                res = wrapped_fun(sample_id, value)
+                if res is not None:
+                    yield res
 
     incremental_save(
         output_json=output_json,
@@ -58,15 +77,16 @@ def incremental_reduce(
     fun: Callable[[Any, dict], dict],
     save_second_interval: int = 10 * 60,
 ) -> None:
-    data = load_json(input_json)
-    assert data
-
-    def data_fun(res):
-        for value in data.values():
-            res = fun(value, res)
-
-    incremental_save(
-        output_json=output_json,
-        data_fun=data_fun,
-        save_second_interval=save_second_interval,
-    )
+    raise NotImplementedError("abc")
+    # data = load_json(input_json)
+    # assert data
+    #
+    # def data_fun(previous_results):
+    #     for value in data.values():
+    #         res = fun(value, previous_results)
+    #
+    # incremental_save(
+    #     output_json=output_json,
+    #     data_fun=data_fun,
+    #     save_second_interval=save_second_interval,
+    # )
