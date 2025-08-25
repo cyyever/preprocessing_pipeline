8206d89487bc95f1a0654fb880df36a8afa9d019
cyy <cyyever@outlook.com>
2025-08-25 09:34:00 +0800

    Fix code

--- cyy_preprocessing_pipeline/signal_handling.py
+++ cyy_preprocessing_pipeline/signal_handling.py
@@ -1,3 +1,4 @@
+import functools
 import os
 import signal
 
@@ -7,17 +8,26 @@ received_signals = set()
 
 
 def __handler(signum, frame) -> None:
-    previous_handler = signal.getsignal(signum)
     log_warning("get signal %s", signum)
     received_signals.add(signum)
-    if previous_handler is not None and not isinstance(previous_handler, int):
-        previous_handler(signum, frame)
 
 
 def check_signal(signum) -> bool:
     return signum in received_signals
 
 
-def setup_signal_handler() -> None:
+def combine_handle(handler1, handler2, signum, frame):
+    handler1(signum, frame)
+    handler2(signum, frame)
+
+
+def setup_signal_handler(signum) -> None:
     log_warning("setup signal handler in pid %s", os.getpid())
-    signal.signal(signal.SIGINT, __handler)
+    previous_handler = signal.getsignal(signum)
+    if previous_handler is not None and not isinstance(previous_handler, int):
+        signal.signal(
+            signum,
+            functools.partial(combine_handle, __handler, previous_handler),
+        )
+    else:
+        signal.signal(signum, __handler)
